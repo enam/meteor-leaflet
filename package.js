4115ee9c0b4ed5db1e5f5ce050fb4dec172b3fb9
Nick Martinelli <nicho.m@gmail.com>
2015-07-19 13:13:17 -0700

    update onUse in package

--- package.js
+++ package.js
@@ -4,14 +4,16 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'meteor package for leaflet.',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/enam/meteor-mapbox.git'
+  git: 'https://github.com/enam/meteor-leaflet.git'
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('meteor-leaflet.js');
+  api.use(['deps', 'underscore'], ['client']);
+  api.addFiles(['meteor-leaflet.js', 'meteor-leaflet.css'], ['client']);
+  api.export('Leaflet', ['client']);
 });
 
 Package.onTest(function(api) {
